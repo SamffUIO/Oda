cdf0b81da2a76cf305e8e981ab99d472f1f2f90c
samff123 <samff@uio.no>
2020-08-18 17:33:49 +0200

    Eksempel på funksjoner

--- Oda_script_2.R
+++ Oda_script_2.R
@@ -1,10 +1,21 @@
-C:\Users\Sam\Documents\Oda\Oda_script_1.R
+#To eksempler på funksjoner
 
-#GjÃ¸r om fahrenhet til celsius funksjon
-f_2_c = function(temp_F) {temp_C = (temp_F - 32)*5/9
-  return (temp_C)}
+#Gjør om fahrenheit til celsius funksjon
+f_2_c = function(temp_F) {
+  temp_C = (temp_F - 32)*5/9
+  return (temp_C)
+  }
+
+#Tester funksjonen vi skrev
+Temp = 32
+f_2_c(Temp)
+
 
 #Regner ut summen av kvadratene
 sum.squares = function(x,y) {
   x^2 + y^2
-}
+  }
+
+#Tester funksjonen
+x = 2; y = 3 
+sum.squares(x,y)
---
+++ Script_Fahrenhet.R
@@ -0,0 +1,4 @@
+f2c = function(temp_F) {
+  temp_C = (temp_F - 32)*5/9
+  return (Temp_C)
+  }
